d8e5d624630075e300c871451d8813546f4f587a
Pavol Otto <pavol.otto@nic.cz>
2014-01-22 18:58:10 +0100

    improved new implementation of serial console and the old serial_console replaced with the new one

--- serial_console.py
+++ serial_console.py
@@ -1,105 +1,241 @@
-import serial
+import threading
+import os
+import termios
 import time
-from re import compile, search
 
-WAITTIME = 1 # waittime should not be too short to be able to detect end of boot messages
-WAITROUNDS = 40
-BOOTWAITROUNDS = 2 * WAITROUNDS
-WRITESLEEP = 0.01
+
+PS1 = "root@turris:/# "
+PS2 = "> "
+UBOOT_PROMPT = "=> "
+
+# following values are all in seconds
+WAITTIME = 0.5
+INIT_EMPTY_WAIT = 5
+INIT_MAX_WAIT = 75
+WRITE_TIMEOUT = 0.1
+
+
+class SCError(IOError):
+    pass
+
 
 class SerialConsole(object):
-    def __init__(self, device, baudrate):
+    def __init__(self, device, baudrate=termios.B115200):
         super(SerialConsole, self).__init__()
         
-        self._device = device
-        self._baudrate = baudrate
-        self._sc = serial.Serial(self._device, self._baudrate, timeout = 1)
+        # self.state = self.UNDEFINED # TODO add states - UNDEFINED, UBOOT, SYSTEM
+        self._running = True
+        self._accept_input = False
+        self.inbuf = ""
+        
+        self._sc = os.open(device, os.O_RDWR)
         
         try:
-            # if booting up, read the log until prompt found
-            counter = BOOTWAITROUNDS
-            lastOutput = "1"
-            consOutput = ""
+            # set termios flags
+            newcc = termios.tcgetattr(self._sc)[6]
             
-            while True:
-                if not lastOutput:
-                    self._sc.write("\r\n")
-                time.sleep(WAITTIME)
-                lastOutput = self._sc.read(self._sc.inWaiting()).replace("\r", "")
-                consOutput += lastOutput
-                match = search(r"(\w+)@(\w+):[\w/~]+#\s*$", consOutput)
-                if match:
-                    self.promptRegexp = match.group(1) + "@" + match.group(2) + r":[\w/~]+#"
-                    break
-                elif counter <= 0:
-                    raise IOError("I've been waiting for prompt too long, giving up...")
-                else:
-                    counter -= 1
-        except Exception:
-            self._sc.close()
-            self._sc = None
+            # this causes read to be non-blocking (along with -ICANON)
+            newcc[termios.VMIN] = 0
+            newcc[termios.VTIME] = 0
+            
+            termios.tcsetattr(self._sc, termios.TCSAFLUSH, [
+                    termios.IGNBRK,
+                    0,
+                    termios.CS8 | termios.CREAD | termios.CLOCAL | baudrate,
+                    0,
+                    baudrate,
+                    baudrate,
+                    newcc,
+                    ])
+        except:
+            # if exception, close tty file
+            try:
+                os.close(self._sc)
+            except:
+                pass
             raise
         
-        # we should get no more output here
-        time.sleep(WAITTIME)
-        oBufChars = self._sc.inWaiting()
-        while oBufChars:
-            self._sc.read(oBufChars)
+        self._inbuf_lock = threading.Lock()
+        self._readThread = threading.Thread(target=self._readWorker)
+        self._readThread.start()
+    
+    def to_system(self):
+        """Read the output (e.g. boot messages) from
+        the console and if nothing is read for INIT_EMPTY_WAIT seconds,
+        it iterpretes it as 'no more boot messages'. Then it echoes \\n
+        to obtain the prompt. If it fails, it retries to send \\n
+        few times.
+        
+        This function either get the router to the state when it accepts
+        commands or raises an SCError.
+        """
+        
+        with self._inbuf_lock:
+            self.inbuf = ""
+        self._accept_input = True
+        
+        totalWaitCycles = int(INIT_MAX_WAIT / WAITTIME)
+        noInputCycle = int(INIT_EMPTY_WAIT / WAITTIME)
+        while noInputCycle > 0 and totalWaitCycles > 0:
             time.sleep(WAITTIME)
-            oBufChars = self._sc.inWaiting()
+            if self.inbuf:
+                noInputCycle = int(INIT_EMPTY_WAIT / WAITTIME)
+                with self._inbuf_lock:
+                    self.inbuf = ""
+            else:
+                noInputCycle -= 1
+            totalWaitCycles -= 1
+                
+                
+        
+        if noInputCycle != 0:
+            # timeouted on totalWaitCycles
+            raise SCError("Too much output, as if booting was in a cycle or so...")
+            
+        # try ten times to obtain the prompt
+        wCounter = 10
+        read = True
+        while wCounter and read:
+            os.write(self._sc, "\n")
+            time.sleep(WAITTIME)
+            if self.inbuf.endswith(PS1):
+                read = False
+            else:
+                wCounter -= 1
+        
+        if wCounter <= 0:
+            raise SCError("Could not obtain prompt.")
+        
+        self._accept_input = False
+    
+    def to_uboot(self):
+        """this function reads output from console and when the text
+        "Hit any key to stop autoboot" is read, it sends ' ' (space) to
+        interrupt the autoboot. Then it waits for $UBOOT_PROMPT.
+        
+        If operating system prompt is found (denoting that os is running)
+        it raises an exception.
+        """
+        
+        with self._inbuf_lock:
+            self.inbuf = ""
+        
+        self._accept_input = True
+        
+        waitCycles = int(INIT_MAX_WAIT / 0.001)
+        waiting = True
+        while waitCycles > 0 and waiting:
+            if self.inbuf.find("Hit any key to stop autoboot") != -1:
+                # send ' ' to interrupt autoboot
+                os.write(self._sc, ' ')
+                waiting = False
+            time.sleep(0.001)
+            waitCycles -= 1
+        
+        
+        
+        wCounter = int(INIT_MAX_WAIT / WAITTIME)
+        while wCounter and not self.inbuf.endswith(UBOOT_PROMPT):
+            os.write(self._sc, "\n")
+            if self.inbuf.find("\n" + PS1) != -1 or self.inbuf.find("\n" + PS2) != -1:
+                self._accept_input = False
+                raise SCError("OS prompt found, restart the device")
+            time.sleep(WAITTIME)
+            wCounter -= 1
+        
+        if waiting:
+            raise SCError("Waiting for uboot messages timeouted.")
+        
+        if wCounter <= 0:
+            raise SCError("Could not get uboot prompt after interrupting autoboot")
+        
+        self._accept_input = False
+    
+    def _readWorker(self):
+        while self._running:
+            tmps = os.read(self._sc, 16).replace("\r", "")
+            if self._accept_input and tmps:
+                with self._inbuf_lock:
+                    self.inbuf += tmps
+            time.sleep(0.0003)
     
     def close(self):
-        self._sc.close()
-        self._sc = None
+        self._running = False
+        self._readThread.join()
+        os.close(self._sc)
     
-    def reopen(self):
-        if self._sc:
-            self._sc.close()
-        self._sc = serial.Serial(self._device, self._baudrate, timeout = 1)
+    def writeLine(self, text):
+        if not self._accept_input:
+            raise ValueError("Allow accepting of input before running writeLine")
+        
+        for c in text[:-1]:
+            if c == "\n":
+                raise ValueError("SerialConsole.writeLine(text) accepts only one line of a text")
+        
+        # write characters
+        inLength = len(self.inbuf)
+        for c in text:
+            os.write(self._sc, c)
+            blocked = True
+            wCounter = int(WRITE_TIMEOUT / 0.001)
+            while blocked:
+                time.sleep(0.001)
+                newPart = self.inbuf[inLength:]
+                if not newPart:
+                    if wCounter > 0:
+                        continue
+                    else:
+                        IOError("Written character not echoed back.")
+                elif newPart == c or c == "\n" and newPart[0] == "\n":
+                    # written character has been read
+                    blocked = False
+                    inLength += 1
+                elif newPart == c + "\n" or newPart == "\n" + c:
+                    blocked = False
+                    inLength += 2
+                else:
+                    raise SCError("unexpected characters in inbuf")
+        
+        return inLength
     
-    def exec_(self, cmd):
-        # cmd should use unix newline (\n)
-        cmd = cmd.replace("\r\n","\n").replace("\r","\n").strip()
+    def exec_(self, text):
+        """Execute given command or script. It must be a
+        single command (output expected only after whole text written),
+        but can span multiple lines.
+        """
         
-        # clear if there is something in the buffer
-        # self._sc.read(self._sc.inWaiting())
-        self._sc.flushInput()
-        self._sc.flushOutput()
-        for x in cmd:
-            time.sleep(WRITESLEEP)
-            self._sc.write(x)
-        time.sleep(WRITESLEEP)
-        self._sc.write("\n")
+        with self._inbuf_lock:
+            self.inbuf = ""
         
-        consOutput = ""
-        endPromptRegexp = compile(self.promptRegexp + r"\s*$")
+        self._accept_input = True
         
-        # wait until a prompt appears
-        counter = WAITROUNDS
-        while True:
-            time.sleep(WAITTIME)
-            consOutput += self._sc.read(self._sc.inWaiting()).replace("\r", "")
-            match = endPromptRegexp.search(consOutput)
-            if match:
-                break
-            elif counter == 0:
-                raise IOError("I've been waiting for output too long, giving up...")
-            else:
-                counter -= 1
+        lines = text.strip().replace("\r", "").split("\n")
+        cmdLen = 0
+        for l in lines[:-1]:
+            # write the line
+            cmdLen += self.writeLine(l + "\n")
+            
+            # wait for prompt PS2
+            wCounter = 50
+            while wCounter and not self.inbuf.endswith(PS2):
+                time.sleep(0.001)
+                wCounter -= 1
+                
+            if wCounter <= 0:
+                raise SCError("Expected prompt not found")
+            
+            cmdLen += len(PS2)
+            
+        cmdLen += self.writeLine(lines[-1] + "\n")
         
-        # cut the command at the beginning and prompt at the end
-        consOutput = consOutput.split("\n")
-        cmdLines = len(cmd.split("\n"))
+        wCounter = 20
+        while wCounter and not self.inbuf.endswith(PS1):
+            time.sleep(0.5)
+            wCounter -= 1
         
-        if cmd.startswith(consOutput[0]):
-            consOutput = "\n".join(consOutput[cmdLines:])
+        if wCounter <= 0:
+            raise SCError("Expected prompt not found")
         
-        match = endPromptRegexp.search(consOutput)
-        while match:
-            consOutput = consOutput[: -len(match.group(0))].rstrip()
-            match = endPromptRegexp.search(consOutput)
+        self._accept_input = False
         
-        return consOutput.strip()
-    
-    def lastStatus(self):
-        return self.exec_("echo $?")
+        return self.inbuf[cmdLen: -len(PS1)]
