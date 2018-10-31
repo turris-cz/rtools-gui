03e2707cd8fcff6693b1c5cc627db4ed9ea2e146
Karel Kočí <karel.koci@nic.cz>
2018-10-31 16:23:45 +0100

    img: add icons

---
+++ img/icons/README.md
@@ -0,0 +1,4 @@
+These icons are not licensed same way as rest of this project. They are from
+Faenza icon pack that is licensed under GPLv3.
+
+URL to upstream project: http://tiheum.deviantart.com/art/Faenza-Icons-173323228
---
+++ img/icons/get.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SIZE=16
+cp /usr/share/icons/Faenza/actions/$SIZE/dialog-ok.png ok.png
+cp /usr/share/icons/Faenza/actions/$SIZE/gnome-run.png run.png
+cp /usr/share/icons/Faenza/actions/$SIZE/gtk-cancel.png cancel.png
+cp /usr/share/icons/Faenza/actions/$SIZE/gtk-close.png unknown.png
+cp /usr/share/icons/Faenza/actions/$SIZE/edit-delete.png fail.png
+cp /usr/share/icons/Faenza/actions/$SIZE/gtk-refresh.png unstable.png
