dc0f601553d7a6ab6d514a26d2c6a834793312d7
Karel Kočí <karel.koci@nic.cz>
2018-11-22 17:18:53 +0100

    Add script to wipe SPI nor memory

---
+++ mox_spi_wipe.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from time import sleep
+import argparse
+from rtools_gui.moxtester import MoxTester
+
+def progress(value):
+    print(value)
+
+def main():
+    parser = argparse.ArgumentParser(prog="moxtester_serial")
+    parser.add_argument('id', metavar='ID', type=int, nargs=1,
+                        help='ID used to connect to moxtester')
+    options = parser.parse_args()
+
+    if options.id[0] < 0 or options.id[0] > 3:
+        exit('ID should be 0 to 3')
+
+    mxt = MoxTester(options.id[0])
+    mxt.selftest()
+
+    if not mxt.board_present():
+        print("Board not inserted")
+        return
+
+    with mxt.spiflash() as flash:
+        flash.reset_device()
+        flash.chip_erase()
+
+
+main()
