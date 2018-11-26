e270ea7cba88cfb8e7f86a56da6e93ccaa06a410
Karel Kočí <karel.koci@nic.cz>
2018-11-26 15:19:29 +0100

    db: centralize select and insert execution

--- rtools_gui/db.py
+++ rtools_gui/db.py
@@ -32,8 +32,16 @@ class _GenericTable:
         self._dbc = db_connection
         self._cur = db_connection.cursor()
 
-    # TODO make all commits to not throw exception and instead write them to
-    # recovery file.
+    def _select(self, sql, values):
+        "SQL select that fails if there is no connection to database"
+        if self._dbc.closed:
+            raise DBException("Database disconnected")
+        self._cur.execute(sql, values)
+
+    def _insert(self, sql, values):
+        "SQL insert that is ensured to go trough"
+        # TODO ensure that it goes trough
+        self._cur.execute(sql, values)
 
 
 class Board(_GenericTable):
@@ -53,7 +61,7 @@ class Board(_GenericTable):
         super().__init__(db_connection)
         self.serial = serial_number
 
-        self._cur.execute(self._SELECT_TYPE, (serial_number,))
+        self._select(self._SELECT_TYPE, (serial_number,))
         res = self._cur.fetchone()
         if res is None:
             raise DBException(
@@ -62,19 +70,19 @@ class Board(_GenericTable):
 
     def mac_wan(self):
         "Returns mac address for wan interface"
-        self._cur.execute(self._SELECT_MAC_WAN, (self.serial,))
+        self._select(self._SELECT_MAC_WAN, (self.serial,))
         res = self._cur.fetchone()
         return None if res is None else res[0]
 
     def mac_sgmii(self):
         "Returns mac address for sgmii interface (moxtet ethernet interface)"
-        self._cur.execute(self._SELECT_MAC_SGMII, (self.serial,))
+        self._select(self._SELECT_MAC_SGMII, (self.serial,))
         res = self._cur.fetchone()
         return None if res is None else res[0]
 
     def revision(self):
         "Numeric identifier of revision"
-        self._cur.execute(self._SELECT_REVISION, (self.serial,))
+        self._select(self._SELECT_REVISION, (self.serial,))
         res = self._cur.fetchone()
         return None if res is None else int(res[0])
 
@@ -83,24 +91,24 @@ class Board(_GenericTable):
         if self.type != "A":
             raise DBException(
                 "Invalid board type for inserting core info: " + str(self.type))
-        self._cur.execute(self._COUNT_CORE_INFO, (self.serial, mem, str(key)))
+        self._select(self._COUNT_CORE_INFO, (self.serial, mem, str(key)))
         if self._cur.fetchone()[0] > 0:
             return  # This one is already recorded
         # TODO what it there is record for this serial number but with
         # different key or memory size
-        self._cur.execute(self._INSERT_CORE_INFO, (self.serial, mem, str(key)))
+        self._insert(self._INSERT_CORE_INFO, (self.serial, mem, str(key)))
 
     def core_mem(self):
         """Returns core memory size for this board. If there is no such key
         then returns None."""
-        self._cur.execute(self._SELECT_CORE_MEM_SIZE, (self.serial,))
+        self._select(self._SELECT_CORE_MEM_SIZE, (self.serial,))
         res = self._cur.fetchone()
         return None if res is None else int(res[0])
 
     def core_key(self):
         """Returns core key for this board. If there is no such key then
         returns None."""
-        self._cur.execute(self._SELECT_CORE_KEY, (self.serial,))
+        self._select(self._SELECT_CORE_KEY, (self.serial,))
         res = self._cur.fetchone()
         return None if res is None else res[0]
 
@@ -123,11 +131,11 @@ class ProgrammerState(_GenericTable):
             resources.mox_imager_hash, resources.secure_firmware_hash,
             resources.uboot_hash, resources.rescue_hash, resources.dtb_hash)
         # Look for existing programmer state
-        self._cur.execute(self._SELECT_PROGRAMMER_ID, state_data)
+        self._select(self._SELECT_PROGRAMMER_ID, state_data)
         state = self._cur.fetchone()
         if state is None:
             # If not found then create new one
-            self._cur.execute(self._INSERT_PROGRAMMER_ID, state_data)
+            self._insert(self._INSERT_PROGRAMMER_ID, state_data)
             state = self._cur.fetchone()
         # Record id of current programmer state
         self.id = state[0]
@@ -144,7 +152,7 @@ class ProgrammerRun(_GenericTable):
     def __init__(self, db_connection, board, programmer_state, programmer_id, steps):
         super().__init__(db_connection)
         self.finished = False
-        self._cur.execute(
+        self._insert(
             self._INSERT_RUN,
             (board.serial, programmer_state.id, programmer_id, steps))
         self.id = self._cur.fetchone()[0]
@@ -153,7 +161,7 @@ class ProgrammerRun(_GenericTable):
         "Mark this run as finished."
         if self.finished:
             raise DBException("Run is already finished")
-        self._cur.execute(self._INSERT_RESULT, (self.id, bool(success)))
+        self._insert(self._INSERT_RESULT, (self.id, bool(success)))
         self.finished = True
 
 
@@ -169,14 +177,14 @@ class ProgrammerStep(_GenericTable):
     def __init__(self, db_connection, run, step_name):
         super().__init__(db_connection)
         self.finished = False
-        self._cur.execute(self._INSERT_STEP, (step_name, run.id))
+        self._insert(self._INSERT_STEP, (step_name, run.id))
         self.id = self._cur.fetchone()[0]
 
     def finish(self, success, message=None):
         "Mark this step as finished."
         if self.finished:
             raise DBException("Step is already finished")
-        self._cur.execute(
+        self._insert(
             self._INSERT_RESULT,
             (self.id, bool(success), message))
         self.finished = True
