a7793563478b98a8672e88488fb1b201302905d3
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2020-02-07 10:43:09 +0100

    settings/omnia_tests_distributors: Use offline base for distributors workflow

--- settings/omnia_tests_distributors.py
+++ settings/omnia_tests_distributors.py
@@ -1,6 +1,12 @@
 # -*- coding: utf8 -*-
 
-from omnia_tests import *
+from omnia_offline import *
+
+SERIAL_CONSOLE['router']['device'] = '/dev/ttyTESTS'
+# tester should not be used in this phase so it can be mocked
+SERIAL_CONSOLE['tester']['mock'] = 'mock/sc_pipe_tester_mock.py'
+
+LOG_APP_FILE = '/var/log/programmer/application-tests.log'
 
 
 WORKFLOW_TESTS_MODULE = 'workflow.tests.omnia_distributors'
