c24c5527df06c632af28235bb90bc432f9166cd4
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2020-02-07 12:25:45 +0100

    distributors: Allow to run all tests
    It is crucial in ethernet tests to run all steps as device tree link
    needs to reset at the end of these tests.
    
    Some other tests can be done without the dependency on prevous one (like
    to test USB3 even if USB2 failed and so)

--- settings/omnia_tests_distributors.py
+++ settings/omnia_tests_distributors.py
@@ -14,4 +14,6 @@ WORKFLOW_TESTS_MODULE = 'workflow.tests.omnia_distributors'
 # In case that the distributor's db is separated from the production
 CHECK_STEPS_BEFORE_TESTS = False
 
+ALWAYS_STOP_ON_FAILURE = False
+
 CUSTOM_INIT_TITLE = u'Testování u distirbutorů'
--- settings/omnia_tests_distributors_usb.py
+++ settings/omnia_tests_distributors_usb.py
@@ -4,4 +4,6 @@ from omnia_tests_distributors import *
 
 WORKFLOW_TESTS_MODULE = 'workflow.tests.omnia_distributors_usb'
 
+ALWAYS_STOP_ON_FAILURE = False
+
 CUSTOM_INIT_TITLE = u'Test USB portů'
